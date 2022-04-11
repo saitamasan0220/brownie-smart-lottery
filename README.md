2e55029e63566a07d4b8b01644749bfd403eb055
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-04-10 22:33:14 -0400

    onlyOwner keyword added from OpenZeppelin

--- README.md
+++ README.md
@@ -3,12 +3,13 @@ npm install -g ganache-cli
 
 brownie networks add development mainnet-fork cmd=ganache-cli host=http://127.0.0.1 fork=https://eth-mainnet.alchemyapi.io/v2/GTxRV303eCPRDQ2Qi2Ru8AW1RANvctkb accounts=10 mnemonic=brownie port=8545
 
-brownie test --network mainnet-fork
+
 ```
 
 ## Brownie CLI Commands
 
 ```
 brownie console --network mainnet-fork
-
+brownie test --network mainnet-fork
+brownie compile
 ```
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,19 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
 
  address payable[] public players;
  uint256 public usdEntryFee;
  AggregatorV3Interface internal ethUsdPriceFeed;
+ enum LOTTERY_STATE {
+  OPEN, 
+  CLOSED,
+  CALCULATING_WINNER
+ }
+ LOTTERY_STATE public lottery_state;
+
 
  constructor(address _priceFeedAddress) public {
   usdEntryFee = 50;
   ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+  lottery_state = LOTTERY_STATE.CLOSED;
  }
- function enter() public payable {
-  // $50 minimum
+
+ function enter() public payable { // $50 minimum
+  require(lottery_state == LOTTERY_STATE.OPEN);
+  require(msg.value >= getEntranceFee(), "Not enough ETH!");
   players.push(msg.sender);
 
  }
@@ -27,18 +38,6 @@ contract Lottery {
  }
 
  function getEntranceFee() public view returns (uint256){ // in wei
-  // (,int price,,,) = ethUsdPriceFeed.latestRoundData();
-
-  // uint256 adjustedPrice = uint256(price) * 10 ** 10; // 18 decimals, conver to wei
-
-  // $50, $2000 / ETH
-  // uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustedPrice;
-
-  // ======================================================
-  // uint256 ethPriceInDollars = getEthPriceInDollars();
-  // uint256 costToEnter = (usdEntryFee / ethPriceInDollars) * 10**18;
-  // return costToEnter;
-
   uint costToEnter = getWeiPriceFromDollar(usdEntryFee);
   return costToEnter;
  }
@@ -50,6 +49,9 @@ contract Lottery {
   return dollarAmountInWei;
  }
 
- function startLottery() public{}
+ function startLottery() public onlyOwner {
+  require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet!");
+  lottery_state = LOTTERY_STATE.OPEN;
+ }
  function endLottery() public {}
 }
